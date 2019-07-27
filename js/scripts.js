25ed978f971bc9f21ec5d70d1e8ff09268508e0f
Gideon-Muriithi <gideongakenge@gmail.com>
2019-07-27 22:24:26 +0300

    Add business and user logic sections in scripts file

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+//Business logic
+function Pizza(size,topping,crust){
+    this.size = size;
+    this.topping = topping;
+    this.crust = crust;
+}
+
+//User Interface logic
+$(document).ready(function(){
+    
+
+});
