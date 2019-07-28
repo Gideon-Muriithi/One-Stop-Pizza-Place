6654a7e11e449c7c9ec34728e909960986ff7d2a
Gideon-Muriithi <gideongakenge@gmail.com>
2019-07-28 22:25:34 +0300

    Add function to append placed order(s)

--- js/scripts.js
+++ js/scripts.js
@@ -5,31 +5,37 @@ function Pizza(size,topping,crust){
     this.crust = crust;
 }
 Pizza.prototype.combineProperties = function() {
-    return this.size + "" + this.topping + "" + this.crust;
+    return "A " + this.size + " pizza " + "with " + this.topping + " topping " + "and " + this.crust + " crust" + ".";
 }
 
 //User Interface logic
 $(document).ready(function(){
-    $("#takeInputs").submit(function(event){
+
+    $("#delivery").click(function(){
+        var location = prompt("Please enter your location.");
+
+      if(location != null){
+      alert("You will be charged Ksh. 250 delivery fee.");
+       alert("Your order will be delivered to your location.");
+       }else{
+          alert("You have cancelled delivery.");
+      }
+    
+   });
+
+     $("#delivery1").click(function(){
+          alert("Then proceed to place your order by checking out on your right.");
+     });
+    
+    $("form#takeInputs").submit(function(event){
        event.preventDefault();
 
        var selectedSize = $("input[name='size']:checked", '#forForm').val();     
-     
-        alert(selectedSize);
+       var selectedTopping = $("input[name='topping']:checked", '#forForm').val();
+       var selectedCrust = $("input[name='crust']:checked", '#forForm').val();
+       var newPizza = new Pizza(selectedSize, selectedTopping,selectedCrust);
        
-       $("#delivery").click(function(){
-           var location = prompt("Please enter your location.");
-
-         if(location != null){
-         alert("You will be charged Ksh. 250 delivery fee.");
-          alert("Your order will be delivered to your location.");
-          }else{
-             alert("You have cancelled delivery.");
-         }
-      });
-
-        $("#delivery1").click(function(){
-             alert("Proceed to place your order by checking out on your right.");
-        });
+       $("ol#properties").append("<li>" + newPizza.combineProperties() + "</li>");
+     
     });
 });
