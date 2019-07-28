cc25a917a01a42f206ac9d819f055d8e3b409ac2
Gideon-Muriithi <gideongakenge@gmail.com>
2019-07-28 11:38:52 +0300

    Add pizza constructor

--- js/scripts.js
+++ js/scripts.js
@@ -7,6 +7,19 @@ function Pizza(size,topping,crust){
 
 //User Interface logic
 $(document).ready(function(){
-    
+    $("#delivery").click(function(){
+      var location = prompt("Please enter your location.");
+
+      if(location != null){
+      alert("You will be charged Ksh. 250 delivery fee.");
+      alert("Your order will be delivered to your location.");
+      }else{
+          alert("You have cancalled delivery.");
+      }
+    });
+
+    $("#delivery1").click(function(){
+        alert("Proceed to place your order by checking out on your right.");
+    });
 
 });
